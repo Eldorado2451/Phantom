8c293982c0f923b2e10cd6ee185dc4dffa298f8e
Eldorado2451 <d.a.frieling@gmail.com>
2025-02-17 13:24:14 +0100

    Cleaned up variables.py

--- variables.py
+++ variables.py
@@ -1,9 +1,10 @@
-x="awesome"         #declares variable x as global variable
+x="awesome"                 #declares global variable x and assigns a sting value
 
-def myfunc():       #function: re-assigns the global variable x
-    global x        #declares that the global x should be used now
-                    # (is only used inside functions. outside of functions, all is global!)
-    x="fantastic"   #assigns a new value to the global x
+def myfunc():               #function: re-assigns the global variable x
+    global x                #declares that the global x should be used now
+                            # (is only used inside functions. outside of functions, all is global!)
+    x="fantastic"           #assigns a new value to the global x
 
-myfunc()            #Run the function above
+myfunc()                    #Run the function defined above
+
 print("Python is " + x)     #prints something outside a function, so takes global
