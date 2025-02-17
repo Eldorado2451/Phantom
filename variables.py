f9aadfffba7a4d663a4309539bddf00ebc88e3c2
Eldorado2451 <d.a.frieling@gmail.com>
2025-02-17 13:21:28 +0100

    Redo of comments

--- variables.py
+++ variables.py
@@ -1,10 +1,9 @@
 x="awesome"         #declares variable x as global variable
 
-def myfunc():       #defines a function
+def myfunc():       #function: re-assigns the global variable x
     global x        #declares that the global x should be used now
                     # (is only used inside functions. outside of functions, all is global!)
     x="fantastic"   #assigns a new value to the global x
 
-myfunc()
-
+myfunc()            #Run the function above
 print("Python is " + x)     #prints something outside a function, so takes global
