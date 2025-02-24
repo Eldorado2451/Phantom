37b5783e1c9581aa97f7245f605ea533952459a9
Eldorado2451 <d.a.frieling@gmail.com>
2025-02-24 17:12:50 +0100

    modified comments based on new learnings about assigning and defining in Python

--- variables.py
+++ variables.py
@@ -1,10 +1,12 @@
-x="awesome"                 #declares global variable x and assigns a sting value
+x="awesome"                 #defines global variable x and assigns a sting value
 
 def myfunc():               #function: re-assigns the global variable x
-    global x                #declares that the global x should be used now
-                            # (is only used inside functions. outside of functions, all is global!)
+    global x                #tells that the global x should be used now
+                            #(is only used inside functions. outside of functions, all is global!)
+    
     x="fantastic"           #assigns a new value to the global x
 
-myfunc()                    #Run the function defined above
+myfunc()                    #Runs the function defined above
 
-print("Python is " + x)     #prints something outside a function, so takes global
+print("Python is " + x)     #prints something outside a function, so takes global, which is re-assigned
+                            # inside the function
