53f27f009c26aeb28cf117375a7f02d3d6f1e933
Eldorado2451 <d.a.frieling@gmail.com>
2025-02-25 13:28:14 +0100

    added comments to ON_OFF_counter.py

--- ON_OFF_counter.py
+++ ON_OFF_counter.py
@@ -1,9 +1,9 @@
-from time import sleep
+from time import sleep          #import sleep for the delay
 
-i = 0
-while i < 5:
+i = 0                           # i is declared globally
+while i < 5:                    # This is the same as while(i<5){...}
     print("ON")
-    sleep(1)
+    sleep(1)                    #delays 1 sec
     print("OFF")
     sleep(1)
-    i += 1
+    i += 1                      #increments i with 1. while is NOT A FUNCTION, so i can be used without declaring "global i" before the increment
