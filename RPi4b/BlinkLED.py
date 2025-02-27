9d021a5c04d8ada8edc200d09d83d25258fbdb4b
Eldorado2451 <d.a.frieling@gmail.com>
2025-02-27 15:56:44 +0100

    range(5) -> range(10)

--- RPi4b/BlinkLED.py
+++ RPi4b/BlinkLED.py
@@ -4,10 +4,11 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)      #Sets BCM pin numbering (GPIOxx), not header numbering
 GPIO.setup(18, GPIO.OUT)    #Sets GPIO18 to output
 
-for i in range(5):
+for i in range(10):
     GPIO.output(18, GPIO.HIGH)
     print("LED is on")
     sleep(1)
     GPIO.output(18, GPIO.LOW)
     print("LED is off")
     sleep(1)
+GPIO.cleanup()
