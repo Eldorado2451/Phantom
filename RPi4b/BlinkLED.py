5f4e3323b74c6d9d90fdf1113f91471db23f0e23
Eldorado2451 <d.a.frieling@gmail.com>
2025-02-27 16:13:07 +0100

    Added comments to BlinkLED.py

--- RPi4b/BlinkLED.py
+++ RPi4b/BlinkLED.py
@@ -1,14 +1,16 @@
+# Function of the code: Drives GPIO18 of a RPi 4b from state ON/OFF. Does this 10x
+
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setmode(GPIO.BCM)      #Sets BCM pin numbering (GPIOxx), not header numbering
-GPIO.setup(18, GPIO.OUT)    #Sets GPIO18 to output
+GPIO.setmode(GPIO.BCM)      # Sets BCM pin numbering (GPIOxx), not header numbering
+GPIO.setup(18, GPIO.OUT)    # Sets GPIO18 to output
 
-for i in range(10):
-    GPIO.output(18, GPIO.HIGH)
-    print("LED is on")
-    sleep(1)
-    GPIO.output(18, GPIO.LOW)
-    print("LED is off")
-    sleep(1)
-GPIO.cleanup()
+for i in range(10):                 # Sets up for loop
+    GPIO.output(18, GPIO.HIGH)      # Writes a HIGH signal (3v3) to GPIO18 
+    print("LED is on")              # Prints the status of the LED (for control)
+    sleep(1)                        # Sleep 1 sec
+    GPIO.output(18, GPIO.LOW)       # Writes a LOW signal (3v3) to GPIO18 
+    print("LED is off")             # Prints the status of the LED (for control)
+    sleep(1)                        # Sleep 1 sec
+GPIO.cleanup()                      # --> cleans up GPIO18: releases control over GPIO18
