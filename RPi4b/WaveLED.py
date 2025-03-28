b2cacdc4d159bf45f0d61d97667544f334817a06
Eldorado2451 <d.a.frieling@gmail.com>
2025-03-28 14:01:30 +0100

    Completed the comments on WaveLED.py including comment header

--- RPi4b/WaveLED.py
+++ RPi4b/WaveLED.py
@@ -1,18 +1,26 @@
 """
-
 # Function of code
 This program is meant to control 3 LEDs in such a way that a wave is achieved from left to right.
 
 # Design method
-This program is based on Phantom/RPi4b/FadeLED.py, the first RPi PWM program written by me. Instead of 1 PWM signals, a total of are used in this one.
+This program is based on Phantom/RPi4b/FadeLED.py, the first RPi PWM program written by me. Instead of 1 PWM signal, a total of 3 are used in this one. 
+The challenge is to change the Duty Cycle of all 3 PWMs in one function instead of just 1. Progressively increasing/decreasing the value of the DutyCycle is
+done using the 'update_dc()' function, run every 10ms within the main while loop.
 
-TODO: tell something about the wave functionality and how it differs from controlling 1 PWM
-TODO: filanlize comments
+# Wave functionality
+The wave functionality works by manipulating the Duty Cycle of the LEDs that follow the 1st LED. By initializing each consecutive Duty Cycle value at -y, -2y etc., 
+it takes time for the respective Duty Cycle to reach 0 when progressively increasing. When the Duty Cycle reaches 0, it can be assigned to the 'pwm.ChangeDutyCycle()' 
+function. This essentially turns ON the next LED n iteration steps later, creating a phase shift in the Duty Cycle sawtooth-curves of the LEDs.
+
+# Important notes !
+- Lists are used to store and organize the values of 'dc' and the iterative value 'a' (dcList & aList)
+- A 'range(len(<list>))' loop is used to read the respective values of 'dc' and 'a' and update them
+- Since len(dcList) == len(aList), the same "loop variable" 'i' can be used
 
 #Author
 Eldorado
-
 """
+
 # Import modules
 import RPi.GPIO as GPIO
 from time import sleep
@@ -31,6 +39,7 @@ pwm1 = GPIO.PWM(PWM_PIN_1, 1000)
 pwm2 = GPIO.PWM(PWM_PIN_2, 1000)
 pwm3 = GPIO.PWM(PWM_PIN_3, 1000)
 
+# initializing PWM Duty Cycle and increment values, including negative strat values for LED 2 & 3
 dc1 = 0
 dc2 = -25
 dc3 = -50
@@ -38,9 +47,11 @@ a1 = 0
 a2 = 0
 a3 = 0
 
+# Organizing dc and a values in lists
 dcList = [dc1, dc2, dc3]
 aList = [a1, a2, a3]
 
+# Upgrading the function of update_dc to check and modify the dc and a lists
 def update_dc():
     global dcList
     global aList
@@ -57,6 +68,8 @@ try:
     pwm1.start(0)
     pwm2.start(0)
     pwm3.start(0)
+
+    # MAIN LOOP
     while True:
         update_dc()
         if dcList[0] >= 0:
