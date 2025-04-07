e19b30736e0b047acc0acd3a8208b42b19ebc605
Eldorado2451 <d.a.frieling@gmail.com>
2025-04-07 15:44:30 +0200

    main.py for Pico made, initial code written, not tested

---
+++ RPiPico/main.py
@@ -0,0 +1,19 @@
+"""
+Not sure if this works yet...
+"""
+
+from machine import Pin
+from time import sleep
+
+led = Pin(19, Pin.OUT)
+
+try:
+    while True:
+    led.toggle()
+    sleep(0.5)
+
+except KeyboardInterrupt:
+    print(" -> execution aborted")
+
+finally:
+    machine.reset()
