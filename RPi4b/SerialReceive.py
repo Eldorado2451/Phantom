d987696144c12ba463b38a9d8f3c67a3a47c7d92
Eldorado2451 <d.a.frieling@gmail.com>
2025-04-10 15:13:13 +0200

    SerialReceive program written

---
+++ RPi4b/SerialReceive.py
@@ -0,0 +1,16 @@
+import serial
+
+ser = serial.Serial('/dev/serial0', baudrate=9600, timeout=1)
+
+print ("Listening on UART0 at 9600 baud...")
+
+try:
+    while True:
+        if ser.in_waiting > 0;
+            data = ser.readline().decode('utf-8', errors='ignore').strip()
+            if data:
+                print(f"Received: {data}")
+except KeyboardInterrupt:
+    print("Exiting.")
+finally:
+    ser.close()
