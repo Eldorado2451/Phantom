b059f165d594ca58e70e771a4e0923adcd883032
Eldorado2451 <d.a.frieling@gmail.com>
2026-05-13 21:39:29 +0200

    Added cosine variables to tune movement

--- RPi4b/2026/NEMA17_SmoothBiRot.py
+++ RPi4b/2026/NEMA17_SmoothBiRot.py
@@ -11,13 +11,19 @@ GPIO.setup(17, GPIO.OUT)    # Sets GPIO17 to output = DIR
 
 LOW_time = 0			# Init LOW_time
 
+min_LOWtime = 0.001
+max_LOWtime = 0.01
+steps = 600
+A = (max_LOWtime - min_LOWtime) / 2
+offset = (max_LOWtime + min_LOWtime) / 2
+
 for i in range(5):
     if i % 2 == 0:
         GPIO.output(17, GPIO.HIGH)  	# Set DIR to HIGH
     else:
         GPIO.output(17, GPIO.LOW)  	# Set DIR to LOW
-    for j in range(200):                # Sets up a for loop to pulse the driver
-        LOW_time = 0.0045 * math.cos(((2 * math.pi) / 200) * j) + 0.0055 # LOW_time is modified by j, ranging from .001 to .01 sec
+    for j in range(steps):                # Sets up a for loop to pulse the driver
+        LOW_time = A * math.cos(((2 * math.pi) / steps) * j) + offset # LOW_time is modified by j, ranging from .001 to .01 sec
         GPIO.output(18, GPIO.HIGH)      # Writes a HIGH signal (3v3) to GPIO18
         # print("HIGH")                 # Prints the status of pin state
         sleep(0.001)                    # Sleep 1 ms
