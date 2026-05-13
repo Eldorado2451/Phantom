1bec5d820afde32849f60642d2e90396bcf2f80d
Eldorado2451 <d.a.frieling@gmail.com>
2026-05-13 19:36:47 +0200

    Changed pulses to 200 and state time to 1ms

--- RPi4b/2026/NEMA17.py
+++ RPi4b/2026/NEMA17.py
@@ -1,4 +1,4 @@
-# Function of the code: Drives GPIO18 of a RPi 4b from state ON/OFF. Pulses 50x
+# Function of the code: Drives GPIO18 of a RPi 4b from state ON/OFF. Pulses 200x when code is run
 # Stepper driver used: A4988
 
 import RPi.GPIO as GPIO
@@ -10,12 +10,12 @@ GPIO.setup(17, GPIO.OUT)    # Sets GPIO18 to output = DIR
 
 GPIO.output(17, GPIO.HIGH)  # Set DIR to HIGH
 
-for i in range(50):                 # Sets up a for loop
+for i in range(200):                # Sets up a for loop
     GPIO.output(18, GPIO.HIGH)      # Writes a HIGH signal (3v3) to GPIO18
     print("HIGH")                   # Prints the status of pin state
-    sleep(0.1)                      # Sleep 100 ms
+    sleep(0.001)                    # Sleep 1 ms
     GPIO.output(18, GPIO.LOW)       # Writes a LOW signal (0v) to GPIO18 
     print("LOW")                    # Prints the status of pin state
-    sleep(0.1)                      # Sleep 100 ms
+    sleep(0.001)                    # Sleep 1 ms
 GPIO.cleanup()                      # --> cleans up GPIO18: releases control over GPIO18
 print("Done!")                      # Print that it is done
