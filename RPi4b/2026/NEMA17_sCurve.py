4150a76a6c644491d2be13f9bdc110f6ae02ed48
Eldorado2451 <d.a.frieling@gmail.com>
2026-05-15 01:22:11 +0200

    cleaned up code

--- RPi4b/2026/NEMA17_sCurve.py
+++ RPi4b/2026/NEMA17_sCurve.py
@@ -1,5 +1,4 @@
-# Function of the code: Drives the stepper driver via GPIO18. The stepper motor moves CW, switches and moves CCW 200 steps. It does this smoothly using a cosine to define the time the STEP signal is low. 
-# Stepper driver used: A4988
+# Function of the code: To be written
 
 import RPi.GPIO as GPIO
 from time import sleep
@@ -9,11 +8,9 @@ GPIO.setmode(GPIO.BCM)      # Sets BCM pin numbering (GPIOxx), not header number
 GPIO.setup(18, GPIO.OUT)    # Sets GPIO18 to output = STEP
 GPIO.setup(17, GPIO.OUT)    # Sets GPIO17 to output = DIR
 
-LOW_time = 0			# Init LOW_time
-
 min_Vel = 500
 max_Vel = 2000
-motionProfile_Steps = 1200
+motionProfile_Steps = 2000
 sCurve_Steps = motionProfile_Steps/4
 
 min_LOWtime = 1/max_Vel
@@ -34,16 +31,16 @@ def calcLOW_time(x):
 
 for i in range(2):
     if i % 2 == 0:
-        GPIO.output(17, GPIO.HIGH)  	                # Set DIR to HIGH
+        GPIO.output(17, GPIO.HIGH)  	        # Set DIR to HIGH
     else:
-        GPIO.output(17, GPIO.LOW)  	                    # Set DIR to LOW
-    for j in range(motionProfile_Steps):                # Sets up a for loop to pulse the driver
-        GPIO.output(18, GPIO.HIGH)                      # Writes a HIGH signal (3v3) to GPIO18
-        # print("HIGH")                 # Prints the status of pin state
-        sleep(min_LOWtime)                    # Sleep 1 ms
-        GPIO.output(18, GPIO.LOW)       # Writes a LOW signal (0v) to GPIO18 
-        # print("LOW")                  # Prints the status of pin state
-        # print(LOW_time)		# = dry run print
-        sleep(calcLOW_time(j))                 # Sleep for LOW_time
-GPIO.cleanup()                      	# Cleans up GPIO18: releases control over GPIO18
-print("Done!")                      	# Print that it is done driving
+        GPIO.output(17, GPIO.LOW)  	            # Set DIR to LOW
+    for j in range(motionProfile_Steps):        # Sets up a for loop to pulse the driver
+        GPIO.output(18, GPIO.HIGH)              # Writes a HIGH signal (3v3) to GPIO18
+        # print("HIGH")                         # Prints the status of pin state
+        sleep(min_LOWtime)                      # Sleep 1 ms
+        GPIO.output(18, GPIO.LOW)               # Writes a LOW signal (0v) to GPIO18 
+        # print("LOW")                          # Prints the status of pin state
+        sleep(calcLOW_time(j))                  # (to be commented)
+    sleep(0.5)                                  # Rest motor before reverse to avoid jerk
+GPIO.cleanup()                      	        # Cleans up GPIO18: releases control over GPIO18
+print("Done!")                      	        # Print that it is done driving
